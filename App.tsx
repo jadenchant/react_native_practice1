75639b363118e68a56cc8e6d827aa3ab89e500cc
Jaden Chant <chant1@stolaf.edu>
2021-04-23 16:05:20 -0500

    added user input

--- App.tsx
+++ App.tsx
@@ -1,10 +1,27 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { View, StyleSheet, Text, TextInput } from "react-native";
 
+// Start App expo start
 export default function App() {
+  const [fname, setFname] = useState("");
+  const [lname, setLname] = useState("");
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Open up App.tsx to start working on your cool app!</Text>
+      <Text>Neeto Burrito</Text>
+      <TextInput
+        style={styles.input}
+        placeholder='First Name'
+        onChange={arg => setFname(arg)}
+        defaultValue={fname}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder='Last Name'
+        onChange={arg => setLname(arg)}
+        defaultValue={lname}
+      />
     </View>
   );
 }
@@ -12,8 +29,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  input: {
+    height: 40,
+    width: 100,
+    backgroundColor: "#ddd",
   },
 });
