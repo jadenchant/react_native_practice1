e2dbe35991b56464e2dedf0433fa390edcd3c39e
Jaden Chant <chant1@stolaf.edu>
2021-04-23 22:05:14 -0500

    updated form

--- App.tsx
+++ App.tsx
@@ -1,27 +1,52 @@
 import React, { useState } from "react";
-import { View, StyleSheet, Text, TextInput } from "react-native";
+import { View, StyleSheet, Text, TextInput, Button } from "react-native";
 
 // Start App expo start
 export default function App() {
   const [fname, setFname] = useState("");
   const [lname, setLname] = useState("");
+  const [err, setErr] = useState("");
+
+  function handleSubmit() {
+    console.log("submit");
+  }
+
+  // Not useful now that I know it's onChangeText and not onChange
+  // function handleChange(input: any, isFname: boolean) {
+  //   if (isFname) {
+  //     if (typeof input === "string") {
+  //       setFname(input);
+  //     } else {
+  //       setErr("First Name is a " + typeof input);
+  //       console.log(input);
+  //     }
+  //   } else {
+  //     if (typeof input === "string") {
+  //       setLname(input);
+  //     } else {
+  //       setErr("Last Name is a " + typeof input);
+  //       console.log(input);
+  //     }
+  //   }
+  // }
 
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your cool app!</Text>
       <Text>Neeto Burrito</Text>
       <TextInput
         style={styles.input}
         placeholder='First Name'
-        onChange={arg => setFname(arg)}
+        onChangeText={input => setFname(input)}
         defaultValue={fname}
       />
       <TextInput
         style={styles.input}
         placeholder='Last Name'
-        onChange={arg => setLname(arg)}
+        onChangeText={input => setFname(input)}
         defaultValue={lname}
       />
+      <Button onPress={handleSubmit} title='Submit' />
+      <Text>{err}</Text>
     </View>
   );
 }
